3d93dac8b413f1f1d87550053cea6cc42f360963
Alexander Dmitriev <dmitriealeksan@yandex.ru>
2023-09-28 14:53:04 +0300

    Mobile first

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ btnToggleTheme.addEventListener('click', () => {
   body.classList.toggle('dark-theme');
   body.classList.toggle('light-theme');
 });
+
+const btnAsideMenu = document.querySelector('.btn-aside-menu');
+const asideMenu = document.querySelector('.aside-menu');
+btnAsideMenu.addEventListener('click', () => {
+  asideMenu.classList.add('show');
+});
+
+const btnCloseAsideMenu = document.querySelector('.aside-menu .close');
+btnCloseAsideMenu.addEventListener('click', () => {
+  asideMenu.classList.remove('show');
+});
